173ed6d98d3413124bdfe1ece386c5fe60d690eb
Vijay Rama Rao D <154080498+Vijay-RamaRao@users.noreply.github.com>
2025-01-06 12:37:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav(){
+    document.getElementById("mysidenav").style.width = "250px";
+}
+
+function closeNav(){
+    document.getElementById("mysidenav").style.width = "0";
+}
+
+window.onload = function() {
+    window.scrollTo(0, 0);
+};
